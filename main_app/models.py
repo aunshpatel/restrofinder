ed85a5213f8f581b4950522c5559140973ca4444
mcf327 <mcf327@gmail.com>
2023-08-14 13:36:10 -0400

    login/logout, index, add restaurant

--- main_app/models.py
+++ main_app/models.py
@@ -3,4 +3,15 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your models here.
 class Restaurant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    opening_time = models.IntegerField()
+    closing_time = models.IntegerField()
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('index')
